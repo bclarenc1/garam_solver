2ba73962a0aca57f1f7ccdf6c1f4da1518cc7e34
benjamin clarenc <benjamin.clarenc@gmail.com>
2025-09-18 14:38:42 +0200

    feat: added main file

---
+++ garam_solver.py
@@ -0,0 +1,41 @@
+# pylint: disable=line-too-long
+# pylint: disable=invalid-name
+
+"""
+Main file to run the Garam solver algorithm.
+
+@author: Benjamin Clarenc
+@date: 2025-09-18
+"""
+
+import os
+import sys
+import argparse
+
+from solver.builder import build_cycle, build_grid
+from solver.solver import solve_cycle, solve_grid
+from solver.display import display_init_and_sol, display_grid
+
+if __name__ == "__main__":
+    sname = os.path.basename(sys.argv[0])
+    parser = argparse.ArgumentParser(
+        description="Solves a full Garam grid or a single cycle (aka 'mini-Garam') that has been inputed.",
+        epilog=("Examples:\n"
+                + f"  python {sname}        -> input a full grid\n"
+                + f"  python {sname} --mini -> input a single cycle\n"),
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-m", "--mini", action='store_true', dest="use_cycle",
+                        help="input a single cycle instead of a full grid")
+
+    args = parser.parse_args()
+
+    if args.use_cycle:
+        scope = "cycle"
+        digits_in, ops = build_cycle()
+        digits_out = solve_cycle(digits_in, ops)
+    else:
+        scope = "grid"
+        digits_in, ops = build_grid()
+        digits_out = solve_grid(digits_in, ops)
+
+    display_init_and_sol(digits_in, digits_out, ops, scope=scope)
